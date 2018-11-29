505a9c92d17db1be73a8f2d8e54e63961aa09d76
Marie <marie@violetco.de>
2018-11-29 12:15:05 -0800

    fix error if query param isn't set

--- searchform.php
+++ searchform.php
@@ -3,7 +3,7 @@
    <?php if (is_search()) : ?>
    <div class="col-8 col-sm-10">
        <label class="sr-only">Search for:</label>
-       <input type="search" class="form-control" placeholder="Search <?php echo get_bloginfo('name') ?>..." value="<?php if ($_GET["s"]){ echo $_GET["s"]; } ?>" name="s" title="Search for:" />
+       <input type="search" class="form-control" placeholder="Search <?php echo get_bloginfo('name') ?>..." value="<?php if (isset($_GET["s"])){ echo $_GET["s"]; } ?>" name="s" title="Search for:" />
    </div>
    <div class="col-4 col-sm-2">
      <input type="submit" class="form-control border btn btn-light borderd" value="Search" />
@@ -11,7 +11,7 @@
    <?php else : ?>
      <div class="col-8 col-md-7">
          <label class="sr-only">Search for:</label>
-         <input type="search" class="form-control" placeholder="Search <?php echo get_bloginfo('name') ?>..." value="<?php if ($_GET["s"]){ echo $_GET["s"]; } ?>" name="s" title="Search for:" />
+         <input type="search" class="form-control" placeholder="Search <?php echo get_bloginfo('name') ?>..." value="<?php if (isset($_GET["s"])){ echo $_GET["s"]; } ?>" name="s" title="Search for:" />
      </div>
      <div class="col-4 col-md-5">
        <input type="submit" class="form-control border btn btn-light borderd" value="Search" />
